e4222e23c3b7ccf2cfcf1e68f53fa034a1d9bded
Gabriel <gabriel_luchtenberg@edu.univali.br>
2018-06-30 18:44:52 -0300

    todas as migrations agora estao com os nomes em ingles

--- database/migrations/2018_05_13_124148_create_products_table.php
+++ database/migrations/2018_05_13_124148_create_products_table.php
@@ -19,13 +19,9 @@ class CreateProductsTable extends Migration
             $table->float('price');
             $table->text('description');
             $table->string('image');
-<<<<<<< HEAD
-            $table->string('categoria');
-=======
             $table->string('category');
->>>>>>> alterações nas migrations
-            $table->date('fabricacao');
-            $table->date('validade');
+            $table->date('fabrication');
+            $table->date('shelf_life');
             $table->timestamps();
         });
     }
